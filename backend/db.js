100138baf944dc3d0ba54169fc7e906649318ac0
Karthik Koppula <karthikkoppula451@gmail.com>
2026-06-05 10:53:50 +0530

    Support local and online database

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.MYSQLHOST || "localhost",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "Karthik@1322",
+    database: process.env.MYSQLDATABASE || "fee_management",
+    port: process.env.MYSQLPORT || 3307
 });
 
 db.connect((err) => {
     if (err) {
-        console.log('Database Error');
+        console.log("Database Error");
         console.log(err);
     } else {
-        console.log('MySQL Connected');
+        console.log("MySQL Connected");
     }
 });
 
